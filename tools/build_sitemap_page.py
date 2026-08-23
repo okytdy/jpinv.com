6ad522766808dc69d293450782cb7a729f23f446
okytdy <okuyama.teddy@gmail.com>
2026-08-23 12:37:46 +0900

    Use Japanese wordmark on Japanese pages (#453)

--- tools/build_sitemap_page.py
+++ tools/build_sitemap_page.py
@@ -20,7 +20,7 @@ import re
 import sys
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-NAV_TAG = '<script src="/assets/nav.js?v=7b0130bdee" defer></script>'
+NAV_TAG = '<script src="/assets/nav.js?v=eae5f964b8" defer></script>'
 
 TITLE_RE = re.compile(r"<title>(.*?)</title>", re.S)
 REFRESH_RE = re.compile(r'http-equiv="refresh"', re.I)
